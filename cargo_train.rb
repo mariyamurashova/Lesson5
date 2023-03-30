3913e6944b757164785aa66ebf51d8c5cfee17de
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-03-30 22:03:44 +0200

    Create cargo_train.rb

---
+++ cargo_train.rb
@@ -0,0 +1,12 @@
+class CargoTrain < Train
+
+  CargoTrain.set_instances
+  CargoTrain.set_register_instances
+  
+  def initialize(number)
+    super
+    @train_type= :cargo 
+    puts "Количество грузовых поездов #{ CargoTrain.instances_add}"
+    puts "Счетчик инстанс = #{register_instance_set}"
+  end
+end
